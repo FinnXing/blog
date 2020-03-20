5ae8f6efbe8aa16876d30b7cc01ec0c678ceaff2
邢斐 <xingfei@sinochem.com>
2020-03-20 21:43:23 +0800

    添加评论

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,6 +25,10 @@ module.exports = {
         text: '标签' // 默认文案 “标签”
       }
     },
+    valineConfig: {
+      appId: 'bd53gbJ5cc0D0shcdgsPRzsG-gzGzoHsz',// your appId
+      appKey: 'Giwu4BSKaxvNoGYD9dVOKMBC', // your appKey
+    },
     // 右侧信息栏头像
     authorAvatar: 'https://img.xing666.net/blog/img/logo.png',
     // 备案
